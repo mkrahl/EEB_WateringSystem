9aafc61bf42c8be978f3aaf49f8237ebf4a4b5b7
diego411 <diego.enrico@directbox.com>
2023-06-15 14:46:40 +0200

    test

--- wateringSystem.py
+++ wateringSystem.py
@@ -8,11 +8,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
  
 def callback(channel):
-        print(channel)
-        if GPIO.input(channel):
-                print("Water Detected!")
-        else:
-                print("NO Water Detected!")
+    print(GPIO.input(channel))
+    if GPIO.input(channel):
+        print("Water Detected!")
+    else:
+        print("NO Water Detected!")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
 GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
