4ff6692ccc5f4a7ff2f978d8cde0c9594beb5bc5
diego411 <diego.enrico@directbox.com>
2023-06-18 13:49:26 +0200

    moisture logging

--- mcptest.py
+++ mcptest.py
@@ -4,6 +4,8 @@ import board
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import csv
+from datetime import datetime
  
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -16,8 +18,25 @@ mcp = MCP.MCP3008(spi, cs)
  
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
+
+headers = ['time', 'adc', 'voltage']
+
+with open('./raw_moisture.csv', 'w') as file:
+     # create the csv writer
+    writer = csv.writer(file)
+    writer.writerow(headers)
  
 while True:
+    # open the file in the write mode
+    with open('./raw_moisture.csv', 'w') as file:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        data = [current_time, chan.value, chan.voltage]
+        
+        # create the csv writer
+        writer = csv.writer(file)
+        writer.writerow(data)
+
     print('Raw ADC Value: ', chan.value)
     print('ADC Voltage: ' + str(chan.voltage) + 'V')
-    time.sleep(2)
+    time.sleep(10)
