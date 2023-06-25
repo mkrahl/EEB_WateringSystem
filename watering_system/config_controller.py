ef82d9f627a5c05bd079cb160e051a9b3113e8fd
diego411 <diego.enrico@directbox.com>
2023-06-25 14:12:57 +0200

    test

--- watering_system/config_controller.py
+++ watering_system/config_controller.py
@@ -9,8 +9,8 @@ def get_desired_moisture():
 def set_desired_moisture(moisture):
     with open(CONF_PATH, 'r+') as f:
         data = json.load(f)
-        data['desired_moisture'] = moisture
-        file.write(json.dumps(data))
+        #data['desired_moisture'] = moisture
+        #file.write(json.dumps(data))
 
 def get_moisture_threshold():
     return get_desired_moisture() * 1.1
