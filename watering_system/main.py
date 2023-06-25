1f589f06ba01e749aee9d58045974da5b5a1e5f3
diego411 <diego.enrico@directbox.com>
2023-06-25 13:30:32 +0200

    add moisture controller

--- watering_system/main.py
+++ watering_system/main.py
@@ -1,13 +1,8 @@
-import busio
-import digitalio
-import board
-import RPi.GPIO as GPIO
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
 import time
 import requests
 import valve_controller
 import config_controller
+import moisture_controller
 import logger
 import os
 from dotenv import load_dotenv
@@ -16,23 +11,12 @@ load_dotenv()
 
 MONITORING_SERVER_URL = os.environ.get("MONITORING_SERVER_URL")
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
- 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.CE0)
- 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
- 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
-
+moisture_controller.setup()
 valve_controller.setup()
 logger.setup()
 
 while True:
-    data = { "adc": chan.value, "voltage": chan.voltage }
+    data = { "adc": moisture_controller.get_adc(), "voltage": moisture_controller.get_voltage() }
     logger.log(data)
 
     requests.post(MONITORING_SERVER_URL + "update", json=data, headers={'Content-Type': 'application/json'})
