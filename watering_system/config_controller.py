55ff30ddf84a9acf1d24da4b1303fcc221f7ebd4
diego411 <diego.enrico@directbox.com>
2023-06-25 14:06:02 +0200

    calibrate moisture

--- watering_system/config_controller.py
+++ watering_system/config_controller.py
@@ -1,11 +1,16 @@
 import json
 
+CONF_PATH = './conf.json'
+
 def get_desired_moisture():
-    with open('./conf.json', 'r') as f:
+    with open(CONF_PATH, 'r') as f:
         return json.load(f)['desired_moisture']
 
-def set_desired_moisture():
-    yield
+def set_desired_moisture(moisture):
+    with open(CONF_PATH, 'w') as f:
+        data = json.load(f)
+        data['desired_moisture'] = moisture
+        file.write(json.dumps(data))
 
 def get_moisture_threshold():
     return get_desired_moisture() * 1.1
--- watering_system/server.py
+++ watering_system/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import config_controller
+import moisture_controller
 
 app = Flask(__name__)
 
@@ -6,4 +8,7 @@ BASE_ROUTE = "/api/v1"
 
 @app.route("/calibrate", methods=["POST"])
 def calibrate():
+    config_controller.set_desired_moisture(
+        moisture_controller.get_adc()
+    )
     return { "response_code": 200 }
