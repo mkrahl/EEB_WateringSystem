1d433833a837211c94e96c143607e0976e14fb84
diego411 <diego.enrico@directbox.com>
2023-06-25 15:31:54 +0200

    add update endpoint in watering server

--- watering_system/server.py
+++ watering_system/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 BASE_ROUTE = "/api/v1"
 
+@app.route("/update", methods=["GET"])
+def update():
+    return { "response_code": 200, data: { "moisture": moisture_controller.get_adc() } }
+
 @app.route("/calibrate", methods=["POST"])
 def calibrate():
     config_controller.set_desired_moisture(
