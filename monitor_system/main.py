10477d70d3e5b40fd6725615ccb4234c7d683f4d
diego411 <diego.enrico@directbox.com>
2023-06-25 14:36:58 +0200

    add calibrate endpoint for monitor server

--- monitor_system/main.py
+++ monitor_system/main.py
@@ -1,19 +1,29 @@
 from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 
 app = Flask(__name__)
 
+MICROCONTROLLER_SERVER_URL = os.environ.get("MICROCONTROLLER_SERVER_URL")
 BASE_ROUTE = "/api/v1"
 
 values = []
 
-@app.route("/", methods=["GET", "POST"])
-def state():
+@app.route("/", methods=["GET"])
+def index():
     return { "response_code": 200, "data": values }
 
-@app.route("/update", methods=["GET", "POST"])
+@app.route("/update", methods=["POST"])
 def update():
-    if request.method == "POST":
-        data = request.get_json()
-        print(data)
-        values.append(data)
-        return { "response_code": 200 }
+    data = request.get_json()
+    print(data)
+    values.append(data)
+    return { "response_code": 200 }
+
+@app.route("/calibrate", methods=["POST"])
+def state():
+    requests.post(MICROCONTROLLER_SERVER_URL + "calibrate")
+    return { "response_code": 200 }
