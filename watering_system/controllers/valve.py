b5f3096df1d0bcbb775f325a60875f99e323a03c
diego411 <diego.enrico@directbox.com>
2023-06-27 20:04:02 +0200

    test

--- watering_system/controllers/valve.py
+++ watering_system/controllers/valve.py
@@ -2,7 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 valve_channel = 21
-is_open = False
+
+TMP = './tmp'
 
 def setup():
     GPIO.setwarnings(False)
@@ -13,15 +14,26 @@ def setup():
 def open(seconds):
     global is_open
     GPIO.output(valve_channel , 0)
-    is_open = True
+    with open(TMP, 'r+') as f:
+        data = json.load(f)
+        data['is_open'] = True
+        f.seek(0)
+        f.write(json.dumps(data))
+        f.truncate()
     print('Opened valve')
     time.sleep(seconds)
 
 def close():
     global is_open
     GPIO.output(valve_channel , 1)
-    is_open = False
+    with open(TMP, 'r+') as f:
+        data = json.load(f)
+        data['is_open'] = False
+        f.seek(0)
+        f.write(json.dumps(data))
+        f.truncate()
     print('Closed valve')
 
 def valve_is_open():
-    return is_open
+    with open(CONF_PATH, 'r') as f:
+        return json.load(f)['is_open']
